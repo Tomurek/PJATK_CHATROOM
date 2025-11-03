4c8ac79a39eaa3ac51fa574735fe4e7f66b4b09d
michalu24 <it.michal.urbanski@gmail.com>
2025-11-03 19:13:48 +0100

    Initial commit

---
+++ src/main/java/org/example/pjatk_chatroom/PjatkChatroomApplication.java
@@ -0,0 +1,13 @@
+package org.example.pjatk_chatroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjatkChatroomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PjatkChatroomApplication.class, args);
+    }
+
+}
